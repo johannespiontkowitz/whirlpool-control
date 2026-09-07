4101cf9497b718fdf88a1763e74f55d365c1348b
johannespiontkowitz <johannespiontkowitz@gmail.com>
2026-09-07 17:17:36 +0200

    added bubble control, reed-switch for pool cover

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import time
 from machine import Pin, ADC
 import onewire, ds18x20
 
-from config import WATER_TEMP_SENSOR_PIN, PUMP1_RELAY_PIN, TARGET_TEMP, HEATER_RELAY_PIN, BUBBLES_POTI_PIN
-from logic import pump_should_run
+from config import WATER_TEMP_SENSOR_PIN, PUMP1_RELAY_PIN, TARGET_TEMP, HEATER_RELAY_PIN, BUBBLES_POTI_PIN, COVER_BUTTON_PIN
+from logic import pump_should_run, control_bubble_level
 
 # Initialize sensors
 ow = onewire.OneWire(Pin(WATER_TEMP_SENSOR_PIN))
@@ -15,6 +15,7 @@ pump1 = Pin(PUMP1_RELAY_PIN, Pin.OUT)
 heater = Pin(HEATER_RELAY_PIN, Pin.OUT)
 bubbles = ADC(Pin(BUBBLES_POTI_PIN))
 bubbles.atten(ADC.ATTN_11DB)  # full 0–3.3 V range
+cover_button = Pin(COVER_BUTTON_PIN, Pin.IN, Pin.PULL_UP)
 
 while True:
     ds.convert_temp()
@@ -23,8 +24,11 @@ while True:
         temp = ds.read_temp(sensor)
         print("temp:", temp, "°C")
 
-        bubble_level = bubbles.read()  # 0–4095 -> potential need to normalize
+        cover_closed = not cover_button.value()  # Assuming active low button
+        print("Cover closed: ", cover_closed)
 
+        bubble_level = bubbles.read()  # 0–4095 -> potential need to normalize
+        bubble_level = control_bubble_level(bubble_level, cover_closed) # if cover is closed, stop bubbles (and LED, not yet implemented)
         print("Bubble level: ", bubble_level)
 
         if pump_should_run(temp, TARGET_TEMP):
